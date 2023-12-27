a0a1850df4a8c1878548888f4f92bfaf21d6ad29
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-12-27 11:07:38 +0700

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,17 @@ import { useState } from "react";
 import ProductFilter from "./components/ProductFilter";
 import ProductGrid from "./components/ProductGrid";
 import SearchBar from "./components/SearchBar";
-import { products } from "./data";
 
 export default function App() {
   const [searchText, setSearchText] = useState("");
   const [category, setCategory] = useState("all");
+
   return (
     <div className="py-16">
-      <div className="max-w-screen-xl mx-auto px-8 space-y-8">
+      <div className="max-w-screen-xl px-8 mx-auto space-y-8">
         <SearchBar searchText={searchText} onSearchTextChange={setSearchText} />
         <ProductFilter category={category} onCategoryChange={setCategory} />
-        <ProductGrid
-          products={products}
-          category={category}
-          searchText={searchText}
-        />
+        <ProductGrid category={category} searchText={searchText} />
       </div>
     </div>
   );
--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,22 +1,47 @@
-import { filterProducts } from "../actions/products";
+import { useEffect, useState } from "react";
 import ProductDetail from "./ProductDetail";
 
-export default function ProductGrid({ products, category, searchText }) {
+export default function ProductGrid({ category, searchText }) {
+  const [products, setProducts] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const sub = category === "all" ? "" : `/category/${category}`;
+    const url = `https://dummyjson.com/products${sub}`;
+    setIsLoading(true);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.products);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [searchText, category]);
+
+  if (isLoading || products === null) return <div>Loading...</div>;
+
+  if (error) return <div>{error.message}</div>;
+
   return (
     <div className="grid grid-cols-3 gap-8">
-      {filterProducts(products, category, searchText).map((product) => (
-        <div className="card bg-base-100 shadow-xl" key={product.id}>
+      {products.map((product) => (
+        <div className="shadow-xl card bg-base-100" key={product.id}>
           <figure>
             <img
               src={product.thumbnail}
               alt={product.title}
-              className="h-52 w-full object-cover"
+              className="object-cover w-full h-52"
             />
           </figure>
           <div className="card-body">
             <h2 className="card-title">{product.title}</h2>
             <p>{product.description}</p>
-            <div className="card-actions justify-end">
+            <div className="justify-end card-actions">
               <ProductDetail
                 id={product.id}
                 title={product.title}
